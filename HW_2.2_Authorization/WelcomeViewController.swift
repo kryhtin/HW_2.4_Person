01997607b9194acf74a799bb7e41507c059ba1b1
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-15 00:04:10 +0600

    Add WelcomeViewController gradient background

--- HW_2.2_Authorization/WelcomeViewController.swift
+++ HW_2.2_Authorization/WelcomeViewController.swift
@@ -18,7 +18,24 @@ class WelcomeViewController: UIViewController {
     welcomeLabel.text = "Welcome, \(welcomeStr ?? "User")!"
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+      setGradientBackground()
+      super.viewWillAppear(animated)
+  }
+  
   @IBAction func pressLogOut() {
     dismiss(animated: true)
+  }
+  
+  private func setGradientBackground() {
+    let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+    let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+    
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [colorTop, colorBottom]
+    gradientLayer.locations = [0.0, 1.0]
+    gradientLayer.frame = self.view.bounds
+    
+    self.view.layer.insertSublayer(gradientLayer, at:0)
   }
 }
