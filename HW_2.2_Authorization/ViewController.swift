d9e742361f56d501ee51539c74b123c932ffe795
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-14 22:08:03 +0600

    Initial Commit

---
+++ HW_2.2_Authorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.2_Authorization
+//
+//  Created by Dmitriy Kryhtin on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
