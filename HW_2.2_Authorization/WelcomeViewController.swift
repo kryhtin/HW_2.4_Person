0159586e4d9980ae4f0ecaebe3beac2174658471
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-14 22:48:21 +0600

    Add ViewControllers and UI elements

---
+++ HW_2.2_Authorization/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  HW_2.2_Authorization
+//
+//  Created by Dmitriy Kryhtin on 14.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
