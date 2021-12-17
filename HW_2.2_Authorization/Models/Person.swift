897f9e831b00e9453e99abf6328b2926a06d0e95
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-17 17:21:04 +0600

    Add user, person models

---
+++ HW_2.2_Authorization/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  HW_2.2_Authorization
+//
+//  Created by Dmitriy Kryhtin on 17.12.2021.
+//
+
+import Foundation
---
+++ HW_2.2_Authorization/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  HW_2.2_Authorization
+//
+//  Created by Dmitriy Kryhtin on 17.12.2021.
+//
+
+import Foundation
