5de19b735e1b81c5c588e7b34d420b088bca0966
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-19 20:06:22 +0600

    Add hobby audio player

--- HW_2.2_Authorization/ViewControllers/HobbyViewController.swift
+++ HW_2.2_Authorization/ViewControllers/HobbyViewController.swift
@@ -14,16 +14,32 @@ class HobbyViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    playSound()
   }
 
-  func playSound() {
-    guard let path = Bundle.main.path(forResource: "Nas ne dogonjat", ofType:"mp3") else { return }
+  @IBAction func pressPlayButtonTatuN() {
+    playSound(name: "Nas ne dogonjat", image: "tatuN")
+  }
+  
+  @IBAction func pressPlayButtonToy() {
+    playSound(name: "toy", image: "toy")
+  }
+  
+  @IBAction func pressPlayButtonTatuA() {
+    playSound(name: "all the think she said", image: "tatuA")
+  }
+  
+  func playSound(name: String, image: String) {
+    guard let path = Bundle.main.path(forResource: name, ofType:"mp3") else { return }
     let url = URL(fileURLWithPath: path)
+    let imageView = UIImageView(frame: UIScreen.main.bounds)
+    imageView.image = UIImage(named: image)
+    imageView.contentMode = .scaleAspectFit
     
     player = AVPlayer(url: url)
     let playerViewController = AVPlayerViewController()
     playerViewController.player = player
+//    playerViewController.view.frame.size = imageView.frame
+    playerViewController.contentOverlayView?.addSubview(imageView)
     self.present(playerViewController, animated: true) {
       playerViewController.player!.play()
     }
